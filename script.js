e43d6775808a71e7ffe70b2efcdb97c0ef8cd191
LucasRalf14 <lucas_w.o.ralf@hotmail.com>
2022-02-26 01:33:33 -0300

    criado bloco de código do carousel

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
     });
+
+    //olw carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                itens: 1,
+                nav: false
+            },
+            600: {
+                itens: 2,
+                nav: false
+            },
+            1000: {
+                itens: 3,
+                nav: false
+            }
+        }
+    });
 });
